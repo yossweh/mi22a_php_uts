c750a176efd2c52bf6a73c15a06799cda56d5934
helleniemlda22 <158249160+helleniemlda22@users.noreply.github.com>
2024-06-12 22:27:28 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php 
+
+  /**
+  *    NIM   : 2257401016
+  *    Nama  : Helen Imeldasari
+  *    Kelas : MI22A
+  */
+
+?>
+
+<style>
+    #menu {
+        margin: 0;
+        padding: 0;
+    }
+
+    #menu a {
+        text-decoration: none;
+        color: black;
+    }
+
+    #menu li {
+        display: inline-block;
+        border: 1px solid gray;
+        padding: 8px;
+    }
+
+    #menu li:hover {
+        background-color: gray;
+    }
+</style>
+
+<ul id="menu">
+        <li><a href="index.php?page=Beranda">Beranda</a></li>
+        <li><a href="NIM_login.php">Login</a></li>
+        <li><a href="NIM_loop.php">Loop</a></li>
+        <li><a href="NIM_profil.php">Profil</a></li>
+</ul>
