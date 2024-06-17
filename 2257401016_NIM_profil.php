1993b31eb58e8275ba5c51d682240f57f0dfcf6e
helleniemlda22 <158249160+helleniemlda22@users.noreply.github.com>
2024-06-17 14:16:46 +0700

    Update 2257401016_NIM_profil.php

--- 2257401016_NIM_profil.php
+++ 2257401016_NIM_profil.php
@@ -12,11 +12,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profil Page</title>
+    <link rel="stylesheet" href="profil.css">
 </head>
 <body>
+<form action="" method="post">
     <h2>Profil</h2>
-    <form action="" method="post">
         <label for="nim">NIM:</label><br>
         <input type="text" id="nim" name="nim"><br>
         <label for="nama">Nama:</label><br>
